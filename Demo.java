64ee234d9e3f4634d0524bcb7cabcce2d904a285
Manish <abc@d.com>
2019-09-24 08:36:18 +0530

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+	public static void main(String []args)
+	{
+		System.out.println("1. Add");
+		System.out.println("2. Sub");
+		System.out.println("3. Mult");
+		System.out.println("4. Div");
+	}
+}
+
+
